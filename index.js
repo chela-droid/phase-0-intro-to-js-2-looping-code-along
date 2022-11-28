7bbc386c388a67fe58e0c5b00907ebc2eef518b5
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-28 08:43:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+
+function writeCards(names, event){
+    let emptyArray= []
+    for(let i=0; i<names.length; i++){
+        const thankYouMessage= `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+     emptyArray.push(thankYouMessage)
+    }
+   return emptyArray
+
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+
+//countdown function using while
+function countDown(){
+    let number=10;
+    while(number>=0){
+        console.log(number--)
+    }
+} 
